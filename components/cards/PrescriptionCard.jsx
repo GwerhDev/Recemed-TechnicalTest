c24c9d8aa5ae20b6eda23a9264fa5903a05d0c4d
Gwerh <gwerh.dev@gmail.com>
2024-08-11 16:39:01 -0400

    styling cards

--- components/cards/PrescriptionCard.jsx
+++ components/cards/PrescriptionCard.jsx
@@ -1,8 +1,22 @@
 export const PrescriptionCard = (props) => {
   const { prescription } = props || null;
+  const { type, doctor } = prescription || null;
+
+  let backgroundColor;
+
+  switch (type) {
+    case "Receta Retenida":
+      backgroundColor = "bg-[#d1f0f9]";
+      break;
+    case "Receta Simple":
+      backgroundColor = "bg-green-100";
+      break;
+    default:
+      backgroundColor = "bg-red-100";
+  }
 
   return (
-    <article className="bg-[#d1f0f9] p-4 rounded-[5px] shadow-md flex flex-col">
+    <article className={`${backgroundColor} p-4 rounded-[5px] shadow-md flex flex-col`}>
       <span className="flex justify-between text-[12px] border-b-2 border-rm-blue-100">
         <p>Folio: <span className="font-semibold">{prescription.code}</span></p>
         <h3 className="font-bold text-rm-blue-100">Receta de Medicamentos</h3>
@@ -10,10 +24,10 @@ export const PrescriptionCard = (props) => {
       <small className="text-[9px] mt-2">{prescription.inserted_at}</small>
       <ul className="flex flex-col gap-1">
         <span className="font-bold text-rm-blue-100">
-          Dr: {prescription.doctor.first_name}
+          Dr: {doctor.first_name}
         </span>
         <p className="text-[12px] font-semibold">
-          {prescription.doctor.speciality}
+          {doctor.speciality}
         </p>
         <p className="text-[12px]">
           código: <span className="font-semibold">{prescription.code}</span>
