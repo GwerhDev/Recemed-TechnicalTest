30c36a099197ce9066732cb59d344bb2162bf923
Gwerh <gwerh.dev@gmail.com>
2024-08-11 22:35:39 -0400

    modularizing environment variables

--- server/config/index.js
+++ server/config/index.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Dejaré los datos expuestos para los fines de revisión,
-// sin embargo son APIs públicas y datos no sensibles.
+// sin embargo son APIs públicas y/o datos no sensibles.
 export const port = process.env.PORT || 3000;
 export const environment = process.env.NODE_ENV || 'development';
 export const apiRecemed = process.env.API_RECEMED || 'http://rec-staging.recemed.cl/api';
